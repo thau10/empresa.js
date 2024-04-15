cc457d7608524152a6f3615729a3dffd2429ddde
Thauane Lourenco <128156748+thau10@users.noreply.github.com>
2024-04-15 14:44:55 -0300

    Add files via upload

---
+++ empresa.js
@@ -0,0 +1,14 @@
+
+let funcionarios =["Jaõa", "Maria","Camila", "Ana", "Pedro","Laura","Tiago","Mariana","Matheus","Sofia"]
+
+console.log(funcionarios)
+funcionarios.push("Mateus")
+funcionarios.push("Isabela")
+funcionarios.push("Rafael")
+funcionarios.push("Camila Fernandes");
+
+console.log(funcionarios);
+
+funcionarios.splice(2,1)//Camila se demitiu
+funcionarios.splice(4,1);//Laura foi para Curitiba
+funconarios. splice
